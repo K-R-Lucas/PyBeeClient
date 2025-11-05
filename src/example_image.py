76ebcb3363e1896350aae4abebd72b381a70cab5
K-R-Lucas <kadenrlucas@yahoo.com.au>
2025-11-05 13:29:26 +1100

    Updated example to use 0.8.2 features.

--- src/example_image.py
+++ src/example_image.py
@@ -4,7 +4,7 @@ from litebee.utils import ImageScanner
 
 
 image = ImageScanner("test.png")
-points = image.get_points()
+points = image.get_points(auto_brightness_exp=1/32, alpha_threshold=200)
 
 show = Case("Python Light Show - Image", 10, 10)
 
@@ -16,7 +16,7 @@ for point, colour in points.items():
         Calibrate(),
         Takeoff(),
         Move3D((point[0], 100, 1000 - point[1])),
-        Move3D((start[0], start[1], 1000 - point[1])),
+        Move3D((start[0], start[1], 1000 - point[1])).add_rgb(colour),
         Land()
     )
 
