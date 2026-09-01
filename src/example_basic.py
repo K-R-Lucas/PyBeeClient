20ca3ed9bbbfc2179dc7588ccd9965dee5b55008
K-R-Lucas <kadenrlucas@yahoo.com.au>
2026-09-01 22:25:05 +1000

    Updated examples to match version 0.9.4 standards.

--- src/example_basic.py
+++ src/example_basic.py
@@ -1,22 +1,28 @@
-from litebee.commands import *
-from litebee.core import Case
 from random import choice, random
 
-show = Case("Python Light Show - Basic", 5, 5)
+from litebee import Case
+from litebee.instructions import *
+
+show = Case("Basic Light Show Example", 5, 5)
 colours = [
-    (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (255, 0, 255),
+    (0, 255, 255),
 ]
 
 for i in range(3):
-    x = 25 + i*225
+    x = 25 + i * 225
     y = 25
     z = 100
 
-    show.add_drone((x, y)).add_commands(
+    show.add_drone((x, y)).add_instructions(
         Calibrate().add_rgb(choice(colours)),
         Takeoff(z),
         Move3D((x, y + random() * 400, z)).add_rgb(choice(colours)),
-        Land()
+        Land(),
     )
 
-show.save()
+show.save()  # If no name is provided, it will use the case name. i.e: "Basic Light Show Example.bin"
--- src/example_image.py
+++ src/example_image.py
@@ -1,23 +1,28 @@
-from litebee.commands import *
-from litebee.core import Case
-from litebee.utils import ImageScanner
+from litebee import Case
+from litebee.instructions import *
+from litebee.types import Rect, Vector3
+from litebee.utils import ImageScanner, scale_points
 
-# Use a dot-art image with a transparent background
-image = ImageScanner("dist/show/goanna.png")
-points = image.get_points(auto_brightness_exp=1/32, alpha_threshold=254)
+# Use a dot-art image with a transparent background (PNG)
+image = ImageScanner("dot_art.png")
+points = image.get_points(alpha_threshold=254)
+scaled_points = scale_points(points, Rect(0.5, 0.5, 9, 9), (9.5, 0.5))
 
 show = Case("Python Light Show - Image", 10, 10)
 
-for point, colour in points.items():
+# Create a drone for every point on the image and assign it to the coordinates.
+for point, colour in scaled_points.items():
     drone = show.add_drone()
     start = drone.start_pos
 
-    drone.add_commands(
+    drone.add_instructions(
         Calibrate(),
         Takeoff(),
-        Move3D((1000*point[0], 100, 1000 * (1 - point[1]))),
-        Move3D((start[0], start[1], 1000 * (1 - point[1]))).add_rgb(colour),
-        Land()
+        Move3D(point),
+        # You can add another Move3D(point, duration=...) here
+        # to make the drones stay in the same position for some time.
+        Move3D(start + Vector3(0, 0, 100)).add_rgb(colour),
+        Land(),
     )
 
 show.save()
